8ba72f4d5416392ba3024fb45872b339306d8cfc
Eugene_Elistratov <em_elistratov@mail.ru>
2023-05-22 15:15:04 +0300

    Задача 34 про Винни-Пуха

--- HW-6/6_2.py
+++ HW-6/6_2.py
@@ -11,17 +11,28 @@
 
 # Вывод: [1, 9, 13, 14, 19]
 
-massiv = [-5, 9, 0, 3, -1, -2, 1,
-        4, -2, 10, 2, 0, -9, 8, 10, -9,
-        0, -5, -5, 7]
-print(massiv)
-massiv_end = []
-index = 0
-for x in massiv:
-    if 5 < x < 15:
-        massiv_end.append(index)
-    index += 1
-print(massiv_end)
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i, end=' ')
 
-# [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
-# [1, 9, 13, 14, 19]
+# 5
+# 15
+# 1 9 13 14 19
+
+# massiv = [-5, 9, 0, 3, -1, -2, 1,
+#         4, -2, 10, 2, 0, -9, 8, 10, -9,
+#         0, -5, -5, 7]
+# print(massiv)
+# massiv_end = []
+# index = 0
+# for x in massiv:
+#     if 5 < x < 15:
+#         massiv_end.append(index)
+#     index += 1
+# print(massiv_end)
+
+# # [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# # [1, 9, 13, 14, 19]
